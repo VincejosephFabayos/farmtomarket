0b1e2fdc609ad915a32e04d3f5e6d8799c4709bf
VincejosephFabayos <vincejosephgaas@gmail.com>
2026-04-09 19:35:02 +0800

    Update database connection details in get_inventory.php

--- get_inventory.php
+++ get_inventory.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=db_cc205test", "root","");
+    $conn = new PDO("mysql:host=sql305.infinityfree.com;dbname=if0_41611143_dbfarmtomarket", "if0_41611143","lo3HSuyJIl");
     
     $stmt = $conn->prepare("SELECT * FROM tb_inventory");
     $stmt->execute();
