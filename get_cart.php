05c6a25d2778c12b47120cdbabb73c6e003e2eff
VincejosephFabayos <vincejosephgaas@gmail.com>
2026-04-09 19:29:06 +0800

    Delete get_cart.php

--- get_cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new PDO("mysql:host=sql305.infinityfree.com;dbname=if0_41611143_dbfarmtomarket", "if0_41611143","lo3HSuyJIl");
-
-if(file_exists($cartFile)) {
-    echo file_get_contents($cartFile);
-} else {
-    echo json_encode([]);
-}
-?>
