7582638dc66cfe4a10ae986cb132cc6ce73ab7d0
VincejosephFabayos <vincejosephgaas@gmail.com>
2026-04-09 19:23:48 +0800

    Add database connection to get_cart.php

--- get_cart.php
+++ get_cart.php
@@ -1,5 +1,5 @@
 <?php
-$cartFile = 'cart_data.json';
+$conn = new PDO("mysql:host=sql305.infinityfree.com;dbname=if0_41611143_dbfarmtomarket", "if0_41611143","lo3HSuyJIl");
 
 if(file_exists($cartFile)) {
     echo file_get_contents($cartFile);
